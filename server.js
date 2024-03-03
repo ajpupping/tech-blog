01e8ecfdbed16bc411970e82b21da9aef448c313
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-02 17:50:40 -0800

    set up database, created connection to sequelize

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
+// const express = require('express');
+// const handlebars = require('express-handlebars');
 
-const app = express();
+const Sequelize = require('sequelize');
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-app.get('/', async (req, res) => {
-    try {
-        const postData = await postMessage.findAll();
-        const posts = postData.map((post) => post.get({ plain: true }));
-
-        res.render('home', { posts });
-    } catch (err) {
-        res.status(500).json(err);
+const sequelize = new Sequelize(
+    'blog_db',
+    'root', 
+    null,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
     }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection to the database has been established successfully.');
+}).catch((err) => {
+    console.error('Unable to connect to the database:', err);
 });
 
-app.get('/dashboard', async (req, res) => {
-    try {
-        const userPostData = await Post.findAll({
-            where: {
-                user_id: req.session.user_id,
-            },
-        });
-        const posts = userPostData.map((post) => post.get({ plain: true }));
-        res.render('dashboard', { posts });
-        } catch (err) {
-            res.status(500).json(err);
-        }
-});
+// const app = express();
+
+// app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+// app.get('/', async (req, res) => {
+//     try {
+//         const postData = await postMessage.findAll();
+//         const posts = postData.map((post) => post.get({ plain: true }));
+
+//         res.render('home', { posts });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+// app.get('/dashboard', async (req, res) => {
+//     try {
+//         const userPostData = await Post.findAll({
+//             where: {
+//                 user_id: req.session.user_id,
+//             },
+//         });
+//         const posts = userPostData.map((post) => post.get({ plain: true }));
+//         res.render('dashboard', { posts });
+//         } catch (err) {
+//             res.status(500).json(err);
+//         }
+// });
